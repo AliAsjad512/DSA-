88425f934a13e00adefceb9fff5875e4a0425028
AliAsjad512 <aa0504503@gmail.com>
2024-10-08 12:47:56 -0400

    New add in code

--- String/ReverseWords.java
+++ String/ReverseWords.java
@@ -6,6 +6,43 @@ package String;
 public class ReverseWords {
 
     public static void main(String[] args){
-        System.out.println("Ali ");
+        String sentence = "How are you";
+        StringBuilder word = new StringBuilder();
+
+        // Use a dynamic array to store words
+        String[] words = new String[sentence.length()]; // Allocate max possible words
+        int wordCount = 0; // Keep track of the number of words
+
+        // Iterate through the characters in the string
+        for (int i = 0; i < sentence.length(); i++) {
+            char ch = sentence.charAt(i);
+            
+            // Check if the character is a space
+            if (ch == ' ') {
+                if (word.length() > 0) { // Only push non-empty words
+                    words[wordCount++] = word.toString(); // Add word to array
+                    word.setLength(0); // Reset for the next word
+                }
+            } else {
+                word.append(ch); // Build the current word
+            }
+        }
+        
+        // Add the last word if there is one
+        if (word.length() > 0) {
+            words[wordCount++] = word.toString();
+        }
+
+        // Now, reverse the words and build the new reversed sentence
+        StringBuilder reversedSentence = new StringBuilder();
+        for (int i = wordCount - 1; i >= 0; i--) {
+            reversedSentence.append(words[i]); // Append words in reverse order
+            if (i > 0) {
+                reversedSentence.append(" "); // Add space between words
+            }
+        }
+
+        // Print the reversed sentence
+        System.out.println("Reversed Sentence: " + reversedSentence.toString());
     }
 }

f17d020c7c969e89376d1b3e9d7b1eb5e05330bd
AliAsjad512 <aa0504503@gmail.com>
2025-02-11 11:44:57 -0500

    element at specific index

---
+++ Array/insertElementatspecifciindex.java
@@ -0,0 +1,28 @@
+class Main {
+    public static void main(String[] args) {
+        int sizeOfArray = 6;
+        int arr[] = {1, 2, 3, 4, 5};
+        int copy[] = new int[sizeOfArray];
+        
+        int index = 2;
+        int element = 90;
+        
+        // Copy the original array into the new array
+        for (int i = 0; i < arr.length; i++) {
+            copy[i] = arr[i];
+        }
+        
+        // Shift elements to the right of the index to make space for the new element
+        for (int i = sizeOfArray - 2; i >= index; i--) {
+            copy[i + 1] = copy[i];
+        }
+        
+        // Insert the new element at the specified index
+        copy[index] = element;
+        
+        // Print the modified array
+        for (int i = 0; i < sizeOfArray; i++) {
+            System.out.print(copy[i] + " ");
+        }
+    }
+}

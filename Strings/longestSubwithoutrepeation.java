25aa1243a4012ba6e1f0037cd4cb2e0749800744
AliAsjad512 <aa0504503@gmail.com>
2025-09-10 16:43:37 -0400

    nested loops

--- Strings/longestSubwithoutrepeation.java
+++ Strings/longestSubwithoutrepeation.java
@@ -3,6 +3,12 @@ public class longestSubwithoutrepeation {
     String s= "dvdf";
     int n = s.length();
     int maxi=0;
-    for(int i=0;i<n;i++)
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            if(allUnique(s,i,j)){
+                maxi=Math.max(maxi,j-i+1);
+            }
+        }
+    }
     
 }

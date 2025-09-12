8ce7376c0abaef0791ae2960b828e806d2efc6e5
AliAsjad512 <aa0504503@gmail.com>
2025-09-11 22:05:08 -0400

    longestreplacemnt

--- Strings/longestreplacemetn.java
+++ Strings/longestreplacemetn.java
@@ -1,51 +1,36 @@
-import java.util.HashMap;
-import java.util.Map;
-
-public class CharacterFrequency {
-    public static void main(String[] args) {
-        String text = "AABABBA";
-        int k=1;
-        int p1=0;
-        int p2=0;
-        String s2="";
-        int max=0;
-        int min=0;
-        int window_size=0;
-        int maxLength=0;
-        Map<Character, Integer> charFrequencies = new HashMap<>();
+class Solution {
+    public int characterReplacement(String s, int k) {
         
-        while(p2<text.length()){
-            s2=s2+text.charAt(p2);
-            window_size++;
-            for (char c : s2.toCharArray()) {
-            charFrequencies.put(c, charFrequencies.getOrDefault(c, 0) + 1);
-        }
-        for (Map.Entry<Character, Integer> entry : charFrequencies.entrySet()) {
-            System.out.println("Character '" + entry.getKey() + "' appears " + entry.getValue() + " times.");
-            max=Math.max(max, entry.getValue());
-            min=Math.max(min, entry.getValue());
-            System.out.println("Max is "+max);
-             System.out.println("Min is "+min);
+
+int p1 = 0; 
+        int maxCount = 0; 
+        int maxLength = 0; 
+        
+        Map<Character, Integer> freq = new HashMap<>();
+        
+        for (int p2 = 0; p2 < s.length(); p2++) {
+            char c = s.charAt(p2);
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
             
             
-            if(window_size-max <=k){
-                maxLength=window_size;
-            }
-            else{
-                maxLength=maxLength-1;
+            maxCount = Math.max(maxCount, freq.get(c));
+            
+            
+            while ((p2 - p1 + 1) - maxCount > k) {
+                char leftChar = s.charAt(p1);
+                freq.put(leftChar, freq.get(leftChar) - 1);
                 p1++;
             }
-        }
-        
-        p2++;
-        
-        
-        
-        
             
+            
+            maxLength = Math.max(maxLength, p2 - p1 + 1);
         }
-System.out.println(maxLength);
-        
+
+       return maxLength;
+
+
+
+
 
         
     }

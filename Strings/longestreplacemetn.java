ba6ba91e576691f17fcb55c0dc83b0aeb8fee66e
AliAsjad512 <aa0504503@gmail.com>
2025-09-14 23:05:45 -0400

    sell and buy

--- Strings/longestreplacemetn.java
+++ Strings/longestreplacemetn.java
@@ -35,3 +35,51 @@ int p1 = 0;
         
     }
 }
+
+
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+       // System.out.println("Try programiz.pro");
+       int[] prices = {2,1,4};
+       int n=prices.length;
+        int maxP=0;
+               // int CurrStock=prices[0];
+        int p1=0;
+        int p2=0;
+        while(p1<n && p2<n){
+            if(prices[p2] - prices[p1] <0){
+                if(p2 <n-1){
+                p1++;
+                p2++;
+                }
+            }
+             else if(prices[p2] > prices[p1]){
+                 System.out.println(prices[p2]);
+                 int result = prices[p2] - prices[p1];
+             
+                maxP=Math.max(maxP,result);
+            }
+            else{
+                p1=p2;
+            
+            }
+           
+            
+            p2++;
+            
+            
+        }
+     //System.out.println(p1);
+        
+        
+        System.out.println(maxP);
+       
+       
+       
+       
+    }
+}

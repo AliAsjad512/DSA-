4653f52854ac6b996408a3dd1bc963bc32e7909e
AliAsjad512 <aa0504503@gmail.com>
2025-10-17 19:51:10 -0400

    Merge Sort

---
+++ DivdiConqLeetcode/MergeSort.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        if (nums.length <= 1) return nums;
+        mergeSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private void mergeSort(int[] arr, int left, int right) {
+        if (left >= right) return;
+        int mid = (left + right) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+
+    private void merge(int[] arr, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+        int i = left, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= right)
+            temp[k++] = (arr[i] <= arr[j]) ? arr[i++] : arr[j++];
+        while (i <= mid) temp[k++] = arr[i++];
+        while (j <= right) temp[k++] = arr[j++];
+
+        System.arraycopy(temp, 0, arr, left, temp.length);
+    }
+}

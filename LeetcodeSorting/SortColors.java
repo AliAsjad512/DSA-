49cdf4ef765dfeed4c81ef1c38079b0ff7b956a0
AliAsjad512 <aa0504503@gmail.com>
2025-10-21 20:08:18 -0400

    Sort Colors

---
+++ LeetcodeSorting/SortColors.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        boolean swapped;
+        for (int i = 0; i < nums.length - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < nums.length - 1 - i; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+}

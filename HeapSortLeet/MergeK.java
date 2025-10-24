dc7b91dc496a1f2f3163a767fb38f129f9854116
AliAsjad512 <aa0504503@gmail.com>
2025-10-24 06:59:26 -0400

    Merge K

---
+++ HeapSortLeet/MergeK.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a.val));
+        for (ListNode node : lists) {
+            if (node != null) pq.offer(node);
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (!pq.isEmpty()) {
+            ListNode node = pq.poll();
+            curr.next = node;
+            curr = curr.next;
+            if (node.next != null) pq.offer(node.next);
+        }
+
+        return dummy.next;
+    }
+}

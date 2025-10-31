7ee282e950260d6dfa77fbb3a539b5791c7c503a
AliAsjad512 <aa0504503@gmail.com>
2025-10-30 20:02:33 -0400

    Tree Preorder

---
+++ PreOder/Nary.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> res = new ArrayList<>();
+        dfs(root, res);
+        return res;
+    }
+
+    private void dfs(Node node, List<Integer> res) {
+        if (node == null) return;
+        res.add(node.val);
+        for (Node child : node.children)
+            dfs(child, res);
+    }
+}

4bcaf5bc59ba872064a8d6af324f839185989fb4
AliAsjad512 <aa0504503@gmail.com>
2025-10-31 21:57:08 -0400

    Post order

---
+++ PostOrder/Travserl.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val) { this.val = val; }
+}
+
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        postorder(root, result);
+        return result;
+    }
+
+    private void postorder(TreeNode node, List<Integer> result) {
+        if (node == null) return;
+        postorder(node.left, result);   // traverse left subtree
+        postorder(node.right, result);  // traverse right subtree
+        result.add(node.val);           // visit root
+    }
+}

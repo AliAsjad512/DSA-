ada6214cf672bafbf8dd56e83e1145e81b959383
AliAsjad512 <aa0504503@gmail.com>
2025-11-11 21:17:05 -0500

    Insert a Node in a BST

---
+++ Binary-Search-Tree/InsertNode.java
@@ -0,0 +1,14 @@
+class BST {
+    Node root;
+
+    Node insert(Node root, int val) {
+        if (root == null) return new Node(val);
+
+        if (val < root.val)
+            root.left = insert(root.left, val);
+        else if (val > root.val)
+            root.right = insert(root.right, val);
+
+        return root;
+    }
+}

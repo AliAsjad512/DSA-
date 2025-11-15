edbcc8df22e3b53ebcd0283d277e91b8b029cb96
AliAsjad512 <aa0504503@gmail.com>
2025-11-14 19:54:43 -0500

    Sort Binary

---
+++ Sorting/SortBinary.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void sortBinaryArray(int[] arr) {
+        int left = 0, right = arr.length - 1;
+
+        while (left < right) {
+            while (arr[left] == 0 && left < right) left++;
+            while (arr[right] == 1 && left < right) right--;
+
+            if (left < right) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+            }
+        }
+    }
+}

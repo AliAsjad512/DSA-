ab2adf8d9648059bb6b48c81299b3225fd00cfb9
AliAsjad512 <aa0504503@gmail.com>
2025-11-15 20:37:57 -0500

    Search in Linked

---
+++ LinkedList/Search.java
@@ -0,0 +1,8 @@
+public boolean search(Node head, int key) {
+    Node temp = head;
+    while (temp != null) {
+        if (temp.data == key) return true;
+        temp = temp.next;
+    }
+    return false;
+}

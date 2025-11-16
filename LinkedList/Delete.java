3227d77d7db411a55a5f32cfca9a41c39f8f2e86
AliAsjad512 <aa0504503@gmail.com>
2025-11-16 18:19:28 -0500

    Delete Tail

---
+++ LinkedList/Delete.java
@@ -0,0 +1,13 @@
+class Solution {
+    public Node deleteTail(Node head) {
+        if (head == null || head.next == null) return null;
+
+        Node temp = head;
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = null;
+        return head;
+    }
+}

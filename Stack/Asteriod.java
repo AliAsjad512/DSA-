3f788c1c6968192cc4d4fea3878e6a06462c660c
AliAsjad512 <aa0504503@gmail.com>
2025-11-21 07:18:28 -0500

    Asteriod

---
+++ Stack/Asteriod.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> st = new Stack<>();
+
+        for (int a : asteroids) {
+            boolean destroyed = false;
+
+            while (!st.isEmpty() && a < 0 && st.peek() > 0) {
+                if (Math.abs(a) > st.peek()) {
+                    st.pop();
+                } else if (Math.abs(a) == st.peek()) {
+                    st.pop();
+                    destroyed = true;
+                    break;
+                } else {
+                    destroyed = true;
+                    break;
+                }
+            }
+
+            if (!destroyed) st.push(a);
+        }
+
+        int[] res = new int st.size()];
+        for (int i = res.length - 1; i >= 0; i--) {
+            res[i] = st.pop();
+        }
+
+        return res;
+    }
+}

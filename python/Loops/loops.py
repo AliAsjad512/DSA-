8414cdd25f748d30daeb9f0b8e8efb6e0aa1e49c
AliAsjad512 <aa0504503@gmail.com>
2025-11-30 17:48:20 -0500

    loops 2

--- python/Loops/loops.py
+++ python/Loops/loops.py
@@ -1,3 +1,13 @@
 animals = ['cat','dog','moneky','monkey']
 for animal in animals:
     print(animal)
+
+    for i in range(0,5,2):
+        print("I am in iteration",i)
+
+
+    
+    animal =['elephant','tiger','lion','giraffe']
+    for idx, animal in enumerate(animal):
+        
+        print('#{}: {}' .format(idx+1, "Animal:", animal))

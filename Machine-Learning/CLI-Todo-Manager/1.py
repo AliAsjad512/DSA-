c20a8c32619dccb9f9c14bb69d28db8dd0110557
AliAsjad512 <aa0504503@gmail.com>
2025-12-03 16:04:31 -0500

    updated

--- Machine-Learning/CLI-Todo-Manager/1.py
+++ Machine-Learning/CLI-Todo-Manager/1.py
@@ -4,17 +4,35 @@ n = int(input("Enter the number of entries: "))
 for _ in range(n):
     key = input("Enter key: ")
     title = input("Enter title: ")
-    priority = input("Enter priority: ")
+    priority = input("Enter priority (high/medium/low): ")
     done = input("Enter done or not (yes/no): ")
 
     tasks[key] = {
         "title": title,
         "priority": priority,
-        "done": done.lower() == "yes"  # convert to boolean
+        "done": done.lower() == "yes"
     }
 
-# list incomplete tasks, titles only
-incomplete_titles = [t["title"] for t in tasks.values() if  t["done"]]
+# list completed tasks
+done_titles = [t["title"] for t in tasks.values() if t["done"]]
 
-for i, title in enumerate(incomplete_titles, 1):
+print("\nThese tasks are done:")
+for i, title in enumerate(done_titles, 1):
     print(i, title)
+
+
+# PRIORITY SORTING
+priority_rank = {"high": 1, "medium": 2, "low": 3}
+
+# make a list of all tasks
+task_list = list(tasks.values())
+
+# sort tasks by priority
+sorted_tasks = sorted(
+    task_list,
+    key=lambda t: priority_rank[t["priority"].lower()]
+)
+
+print("\nTasks sorted by highest priority:")
+for t in sorted_tasks:
+    print(f"{t['priority']} - {t['title']}")

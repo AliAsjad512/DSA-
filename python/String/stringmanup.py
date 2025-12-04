c0df3ddfb50bdc269ad187c8a23dd814d275f229
AliAsjad512 <aa0504503@gmail.com>
2025-12-04 13:31:13 -0500

    String project

--- python/String/stringmanup.py
+++ python/String/stringmanup.py
@@ -1,2 +1,10 @@
 print("Hello, World!\n Hello,Universe!")
 
+print("hello"+" Ali")
+
+print("Welcome to the Band Name Generator.")
+city=input("What's the name of the city you grew up in? \n")
+print(city)
+pet=input("What's your pet's name? \n")
+print(pet)
+print("Your band name could be " + city + " " + pet)

d4ee158a6e9dae0b42436b905ae27499c39dbfaa
AliAsjad512 <aa0504503@gmail.com>
2025-12-05 21:17:58 -0500

    MaxWinode

---
+++ python/List/list1.py
@@ -0,0 +1,4 @@
+state_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island"]
+
+if "New York" in state_of_america:
+    print("New York is in the list.")

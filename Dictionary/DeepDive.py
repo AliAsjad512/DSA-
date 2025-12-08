b3c0c402fcef5e9af916d3bea1ebf4c4479fe8eb
AliAsjad512 <aa0504503@gmail.com>
2025-12-07 20:41:03 -0500

    deep div

---
+++ Dictionary/DeepDive.py
@@ -0,0 +1,11 @@
+pro_dictionary = {
+    "name": "Laptop",
+    "price": 1200,
+    "specs": {
+        "processor": "Intel i7",
+        "ram": "16GB",
+        "storage": "512GB SSD"
+    }
+}
+
+pro_dictionary["specs"]["ram"]

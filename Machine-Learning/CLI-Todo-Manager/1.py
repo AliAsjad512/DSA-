6f490d2486ae8765449043b4a776130e34f64577
AliAsjad512 <aa0504503@gmail.com>
2025-12-07 20:21:05 -0500

    hello world

--- Machine-Learning/CLI-Todo-Manager/1.py
+++ Machine-Learning/CLI-Todo-Manager/1.py
@@ -3,7 +3,7 @@ n = int(input("Enter the number of entries: "))
 
 for _ in range(n):
     key = input("Enter key: ")
-    title = input("Enter title: ")
+    title = float(input("Enter title: "))
     priority = input("Enter priority (high/medium/low): ")
     done = input("Enter done or not (yes/no): ")
 
@@ -25,3 +25,29 @@ for i, title in enumerate(done_titles, 1):
 priority_rank = {"high": 1, "medium": 2, "low": 3}
 
 # make a list of all tasks
+
+
+# Convert tasks dictionary → list
+task_list = []
+for key in tasks:
+    task_list.append(tasks[key])
+
+# Create paired list: [priority_number, task]
+paired_list = []
+for task in task_list:
+    rank = priority_rank[task["priority"].lower()]
+    paired_list.append([rank, task])
+
+# Sort by the priority number
+paired_list.sort()
+
+# Extract sorted tasks
+sorted_tasks = []
+for pair in paired_list:
+    sorted_tasks.append(pair[1])
+
+# Print sorted tasks
+print("\nTasks sorted by highest priority:")
+for t in sorted_tasks:
+    print(t["priority"], "-", t["title"])
+
---
+++ Strings/string.py
@@ -0,0 +1,4 @@
+data = 'hello world'
+print(data[0])
+print(len(data))
+print(data)

352edd343dff35727c7a53285eab7527725d2db0
AliAsjad512 <aa0504503@gmail.com>
2025-12-09 21:20:23 -0500

    new dict

--- python/Dictionary/dict.py
+++ python/Dictionary/dict.py
@@ -1,3 +1,12 @@
 d = {'cat': 'cute', 'dog': 'furry'}
 print(d['cat'])  # Output: cute
 print('cat' in d)  # Output: True
+
+mydict={ 'ali' :1, 'asjad' :2, 'abbas':3}
+print("A VALUE :%d" % mydict['asjad'])
+mydict['a']=11
+print("A VAUE: %d" %mydict['ali'])
+print("keys.%s" %mydict.keys())
+print("VALUE:%s" %mydict.values())
+for key in mydict.keys():
+    print(mydict[key])

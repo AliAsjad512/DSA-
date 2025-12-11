11e3575be5ffeff590334d05486c376b5aa2743b
AliAsjad512 <aa0504503@gmail.com>
2025-12-10 21:30:58 -0500

    travel

--- python/Dictionary/nested.py
+++ python/Dictionary/nested.py
@@ -5,8 +5,37 @@ capitals = {
 }
 
 # Nested List in Dictionary
+# travel_log = {
+#     "France": ["Paris", "Lille", "Marseille"],
+#     "Germany": ["Berlin", "Hamburg", "Munich"],
+#     "Italy": ["Rome", "Milan", "Naples"]
+# }
+
+# for country, cities in travel_log.items():
+    
+#     for city in cities:
+#       if city == "Lille":
+#        print(city)
+
+#print(travel_log["France"][1])
+    
+nested_list = ["A", "B", ["C","D"]]
+print(nested_list[2][1])
+
+
 travel_log = {
-    "France": ["Paris", "Lyon", "Marseille"],
-    "Germany": ["Berlin", "Hamburg", "Munich"],
-    "Italy": ["Rome", "Milan", "Naples"]
+  "France":{
+    "num_times_visited": 12,
+    "cities_visited": ["Paris", "Lille", "Marseille"]
+  },
+  "Germany":{
+    "num_times_visited": 5,
+    "cities_visited": ["Berlin", "Hamburg", "Munich"]
+  },
+  "Italy":{
+    "num_times_visited": 3,
+    "cities_visited": ["Rome", "Milan", "Naples"]
+  }
 }
+
+print(travel_log["Germany"]["cities_visited"][2])

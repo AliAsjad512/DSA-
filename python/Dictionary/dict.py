42cbc375de32a682dc81dcf33859d240565465f8
AliAsjad512 <aa0504503@gmail.com>
2025-12-10 20:56:51 -0500

    student grades

--- python/Dictionary/dict.py
+++ python/Dictionary/dict.py
@@ -10,3 +10,28 @@ print("keys.%s" %mydict.keys())
 print("VALUE:%s" %mydict.values())
 for key in mydict.keys():
     print(mydict[key])
+
+
+
+    student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+
+student_grades = {}
+
+for student, score in student_scores.items():
+    print(student)
+    if score >= 90:
+        student_grades[student] = "Outstanding"
+    elif score >= 80:
+        student_grades[student] = "Exceeds Expectations"
+    elif score >= 70:
+        student_grades[student] = "Acceptable"
+    else:
+        student_grades[student] = "Fail"
+
+print(student_grades)

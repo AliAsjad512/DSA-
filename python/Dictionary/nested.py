cfb8c034cb37dfc6a2e53413502b59bb30161753
AliAsjad512 <aa0504503@gmail.com>
2025-12-10 21:01:13 -0500

    nested

---
+++ python/Dictionary/nested.py
@@ -0,0 +1,12 @@
+capitals = {
+    'France': 'Paris',
+    'Germany': 'Berlin',
+    'Italy': 'Rome'
+}
+
+# Nested List in Dictionary
+travel_log = {
+    "France": ["Paris", "Lyon", "Marseille"],
+    "Germany": ["Berlin", "Hamburg", "Munich"],
+    "Italy": ["Rome", "Milan", "Naples"]
+}

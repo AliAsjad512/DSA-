844774143b1c38a3e9fc95c0397a821dfda5401d
AliAsjad512 <aa0504503@gmail.com>
2025-12-13 20:45:41 -0500

    print second subject from dictionary

---
+++ python/Dictionary/Mypractise.py
@@ -0,0 +1,11 @@
+
+
+
+
+#print the subject "Physics" from the list of subjects in the dictionary
+student = {
+    "name": "Ali",
+    "subjects": ["Math", "Physics", "CS"]
+}
+
+print(student["subjects"][1])

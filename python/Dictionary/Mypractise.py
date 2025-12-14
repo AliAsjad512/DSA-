1cae405b2d26bf4f59f5b97408c8db00488fd20a
AliAsjad512 <aa0504503@gmail.com>
2025-12-14 15:05:08 -0500

    skills

--- python/Dictionary/Mypractise.py
+++ python/Dictionary/Mypractise.py
@@ -1,33 +1,54 @@
 
-
-
-
-#print the subject "Physics" from the list of subjects in the dictionary
-student = {
-    "name": "Ali",
-    "subjects": ["Math", "Physics", "CS"]
+response = {
+    "status": 200,
+    "data": {
+        "users": [
+            {
+                "id": 1,
+                "name": "Ali",
+                "skills": ["Python", "AWS"]
+            },
+            {
+                "id": 2,
+                "name": "Sara",
+                "skills": ["Java", "SQL"]
+            }
+        ]
+    }
 }
 
-print(student["subjects"][1])
+for skill in response['data']['users']['skills']:
+  print(skill)
 
 
-students = [
-    {"name": "Ali", "age": 25},
-    {"name": "Sara", "age": 24}
-]
-print(students[1]["age"])
+# #print the subject "Physics" from the list of subjects in the dictionary
+# student = {
+#     "name": "Ali",
+#     "subjects": ["Math", "Physics", "CS"]
+# }
+
+# print(student["subjects"][1])
 
 
-employee = {"emp1": {"name": "Ali", "role": "Dev"}}
-employee["emp1"]["salary"] = 70000
-print(employee)
+# students = [
+#     {"name": "Ali", "age": 25},
+#     {"name": "Sara", "age": 24}
+# ]
+# print(students[1]["age"])
 
-#update data of first student 
-students = [
-    {"name": "Ali", "marks": 85},
-    {"name": "Sara", "marks": 92}
-]
-students[0]["name"]="Asjad"
-students[0]["marks"]=100
-for student in students:
- print(student)
+
+# employee = {"emp1": {"name": "Ali", "role": "Dev"}}
+# employee["emp1"]["salary"] = 70000
+# print(employee)
+
+# #update data of first student 
+# students = [
+#     {"name": "Ali", "marks": 85},
+#     {"name": "Sara", "marks": 92}
+# ]
+# students[0]["name"]="Asjad"
+# students[0]["marks"]=100
+# for student in students:
+#  print(student)
+
+

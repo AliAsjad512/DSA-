13c38c4fd818d13ccd656efdb8ddcc59f3ee76d0
AliAsjad512 <aa0504503@gmail.com>
2025-12-13 21:11:58 -0500

    company data

---
+++ python/Dictionary/bid.py
@@ -0,0 +1,36 @@
+# print("\n" *100)
+# input("Press Enter to continue...")
+
+company = {
+    "name": "TechCorp",
+    "employees": [
+        {"name": "Ali", "role": "Dev"},
+        {"name": "Sara", "role": "QA"}
+    ]
+}
+
+# print(company["employees"])
+# print(company["employees"][1]["name"]["role"])
+# for employee in company["employees"]:
+#     if employee['role']=='Dev':
+#      print(employee['name'],employee['role'])
+
+
+     data = {
+    "users": [
+        {
+            "id": 1,
+            "skills": ["Python", "AWS"]
+        }
+    ]
+}
+
+print(data["users"][0]["id"][0])
+
+company = {
+    "employees": [
+        {"name": "Ali", "skills": ["Python", "AWS"]},
+        {"name": "Sara", "skills": ["Java", "SQL"]}
+    ]
+}
+print(company["employees"][1]["skills"][0])

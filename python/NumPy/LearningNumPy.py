bbb60722e8e2cee68fcaef13c2c88196867ddcbc
AliAsjad512 <aa0504503@gmail.com>
2025-12-16 06:55:52 -0500

    combined array

--- python/NumPy/LearningNumPy.py
+++ python/NumPy/LearningNumPy.py
@@ -17,3 +17,11 @@
 # Memory efficient
 
 # C-backed operations
+
+import numpy as np
+arr = np.array([1, 2, 3])
+arr * 2     # ✅ [2 4 6]
+
+arr = np.array([1, 2, 3])
+arr + np.array([4, 5, 6])  # ✅ [5 7 9]
+print(arr.shape)  # ✅ (3,)

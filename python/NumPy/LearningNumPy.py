1aa7febdf8719a06476f88d3e0804ab2958dc8a1
AliAsjad512 <aa0504503@gmail.com>
2025-12-16 07:08:42 -0500

    square Even numbers

--- python/NumPy/LearningNumPy.py
+++ python/NumPy/LearningNumPy.py
@@ -25,3 +25,7 @@ arr * 2     # ✅ [2 4 6]
 arr = np.array([1, 2, 3])
 arr + np.array([4, 5, 6])  # ✅ [5 7 9]
 print(arr.shape)  # ✅ (3,)
+
+arr = np.array([1,2,3,4,5,6])
+arr=arr[arr%2==0] ** 2
+print(arr)  # ✅ [ 4 16 36]

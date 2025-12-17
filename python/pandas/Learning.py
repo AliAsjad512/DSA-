efd6b37710d460852a9b3a943714ee76fed8cdf6
AliAsjad512 <aa0504503@gmail.com>
2025-12-17 06:50:06 -0500

    telecom

---
+++ python/pandas/Learning.py
@@ -0,0 +1,14 @@
+df.head()
+df.tail()
+df.info()
+df.describe()
+df.shape
+df.columns
+df.dtypes
+
+operators = ['AT&T', 'Verizon', 'T-Mobile US', 'US Cellular']
+revenue = [171.76, 128.29, 68.4, 4.04]
+
+#creating a Series from lists
+telecom = pd.Series(revenue, index=operators)
+telecom

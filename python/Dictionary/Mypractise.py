702af2848eaacbbd5d019333d6e65f69957c33eb
AliAsjad512 <aa0504503@gmail.com>
2025-12-21 09:32:34 -0500

    students data

--- python/Dictionary/Mypractise.py
+++ python/Dictionary/Mypractise.py
@@ -150,3 +150,55 @@ for i in range(num_students):
 # Print the resulting dictionary
 print("\nStudent Data Dictionary:")
 print(user_data)
+
+# Initialize an empty dictionary
+user_data = {}
+
+# Ask the user how many students
+num_students = int(input("Enter the number of students: "))
+
+# Loop for each student
+for i in range(num_students):
+    print(f"\n--- Student {i+1} ---")
+
+    student_id = int(input("Enter student ID: "))
+    student_name = input("Enter student name: ")
+
+    
+    num_subjects = int(input("Enter total number of subjects: "))
+
+    
+    subjects_dict = {}
+
+    
+    for j in range(num_subjects):
+        subject_name = input(f"Enter name of subject {j+1}: ")
+        grade = int(input(f"Enter grade for {subject_name}: "))
+        subjects_dict[subject_name] = grade
+        total=sum(subjects_dict.values())
+        average= total / len(subjects_dict)
+
+    # Store all student info
+    user_data[student_id] = {
+        "name": student_name,
+        "subjects": subjects_dict,
+         "total" :total,
+          "average" :average
+    }
+
+    for key,value in user_data.items():
+          print(value['name'])
+        #  if(value['subjects']):
+        # #   print(value['subjects'])
+        #   for ke,subj in value['subjects'].items():
+        #       print(ke, subj)
+
+
+
+      # if (value=='subjects'):
+      #  for subj,marks in value:
+        #  print(value);
+
+# Print the resulting dictionary
+# print("\nStudent Data Dictionary:")
+# print(user_data)

425e056989a972b20506cba2148149b1302a9ce7
AliAsjad512 <aa0504503@gmail.com>
2025-12-29 20:08:13 -0500

    car data

--- python/pandas/tensorFlow.py
+++ python/pandas/tensorFlow.py
@@ -9,3 +9,6 @@ print(colors)
 # DataFrame is two dimensiona
 car_data = pd.DataFrame({"Car make": series, "colors":colors})
 print(car_data)
+
+car_sales=pd.read_csv("car-sales.csv")
+print(car_sales)

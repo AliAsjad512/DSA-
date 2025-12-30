fee2a6b1488eee8b8a140eeabfbcda76c80aae51
AliAsjad512 <aa0504503@gmail.com>
2025-12-29 19:51:25 -0500

    pn and Df

--- python/pandas/tensorFlow.py
+++ python/pandas/tensorFlow.py
@@ -1,2 +1,11 @@
 import pandas as pd
 series = pd.Series(["BMW", "Toyota", "Honda"])
+print(series)
+
+colors= pd.Series(["Red","Blue", "White"])
+print(colors)
+
+# Series is one dimensional 
+# DataFrame is two dimensiona
+car_data = pd.DataFrame({"Car make": series, "colors":colors})
+print(car_data)

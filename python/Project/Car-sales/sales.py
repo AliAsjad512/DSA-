20725ff0d03d9bd533b12382a37657ce7d94febe
AliAsjad512 <aa0504503@gmail.com>
2026-01-01 16:12:04 -0500

    a

--- python/Project/Car-sales/sales.py
+++ python/Project/Car-sales/sales.py
@@ -14,3 +14,20 @@ print('Math score mean',df['Math'].mean())
 print('Science score mean',df['Science'].mean())
 print('English score mean',df['English'].mean())
 print(df)
+
+print('\n\n\ncar sale mean')
+
+car_sales['Price'] = (
+    car_sales['Price']
+    .astype(str)
+    .str.replace('$', '', regex=False)
+    .str.replace(',', '', regex=False)
+    .str.strip()
+)
+
+car_sales['Price'] = pd.to_numeric(car_sales['Price'], errors='coerce')
+
+print(car_sales['Price'].mean())
+print(car_sales["Price"].max())
+print(car_sales["Price"].min())
+car_sales[car_sales["Make"] == "Toyota"]["Price"].max()

079f567aaa289dcd212f59be129af8f2a943d51b
AliAsjad512 <aa0504503@gmail.com>
2026-01-05 20:24:01 -0500

    NumPy

---
+++ python/Project/Numpy/num.py
@@ -0,0 +1,14 @@
+import numpy as np
+# DataTypes & Attribute
+#NumPy's main datatype is ndarray
+al = np.array([1,2,3])
+type(al)
+a2 = np.array([[1,2.0, 3.3],
+               [4, 5, 6.5]])
+a3 = np.array([[[1 , 2, 3],
+                [4, 5, 6], 
+                 [7, 8,9]],
+                [[10, 11, 12],
+                 [13,14,15],
+                  [ 16, 17, 18]]
+               ])

86b863a8cec50e28cde35bbf03a35ac103b9d41a
Ali Abbas <aa0504503@gmail.com>
2026-01-25 14:11:52 -0500

    append

--- LinkedList/append.py
+++ LinkedList/append.py
@@ -7,3 +7,13 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+
+        def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node

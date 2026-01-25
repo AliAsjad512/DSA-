f61bbade55c12e46c3195bd84eded7eff3838a2a
Ali Abbas <aa0504503@gmail.com>
2026-01-25 14:10:38 -0500

    linked list

---
+++ LinkedList/append.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+# Linked List class
+class LinkedList:
+    def __init__(self):
+        self.head = None

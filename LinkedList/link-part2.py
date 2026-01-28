d2223946483b91fb4b41fe84f03f186c4ded3226
Ali Abbas <aa0504503@gmail.com>
2026-01-28 18:05:58 -0500

    linked

---
+++ LinkedList/link-part2.py
@@ -0,0 +1,29 @@
+# Node class for Linked List
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+# Linked List class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    # Utility function to add a node at the end
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+    
+    # Utility function to print the linked list
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")

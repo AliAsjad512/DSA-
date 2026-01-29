3176c65ed0365dbc455a7f2b63bf2570c0be1fc6
Ali Abbas <aa0504503@gmail.com>
2026-01-29 14:05:12 -0500

    reverse linked list

--- LinkedList/link-part2.py
+++ LinkedList/link-part2.py
@@ -187,5 +187,19 @@ def join_linked_lists(head1, head2):
 
 # ========== 7. Reverse a linked list ==========
 def reverse_linked_list(head):
-    """
-    Reverse the linked li
+    prev = None
+    current = head
+    
+    while current:
+        # Store the next node
+        next_node = current.next
+        
+        # Reverse the link
+        current.next = prev
+        
+        # Move pointers forward
+        prev = current
+        current = next_node
+    
+    # prev is the new head
+    return prev

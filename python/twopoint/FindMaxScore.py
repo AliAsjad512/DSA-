eeb3be089d4a62477fbbae6be2ba6c87567c7bc1
Ali Abbas <aa0504503@gmail.com>
2026-02-21 12:54:46 -0500

    maxScore 2

--- python/twopoint/FindMaxScore.py
+++ python/twopoint/FindMaxScore.py
@@ -8,3 +8,20 @@ def maximumScore(self, nums, k):
         left = right = k
         min_val = nums[k]
         max_score = min_val
+
+        while left > 0 or right < len(nums) - 1:
+            # Expand in direction of larger value
+            if left == 0:
+                right += 1
+                min_val = min(min_val, nums[right])
+            elif right == len(nums) - 1:
+                left -= 1
+                min_val = min(min_val, nums[left])
+            elif nums[left - 1] > nums[right + 1]:
+                left -= 1
+                min_val = min(min_val, nums[left])
+            else:
+                right += 1
+                min_val = min(min_val, nums[right])
+            
+            max_score = max(max_score, min_val * (right - left + 1))

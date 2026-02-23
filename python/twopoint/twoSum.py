580a9305cdb0f92d73dec4162564b42ec806a49c
Ali Abbas <aa0504503@gmail.com>
2026-02-23 16:41:50 -0500

    two sum done

--- python/twopoint/twoSum.py
+++ python/twopoint/twoSum.py
@@ -9,3 +9,11 @@
         
         while left < right:
             current_sum = numbers[left] + numbers[right]
+             if current_sum == target:
+                return [left + 1, right + 1]  # 1-indexed as per problem
+            elif current_sum < target:
+                left += 1  # Need larger sum, move left pointer right
+            else:
+                right -= 1  # Need smaller sum, move right pointer left
+        
+        return []  # No solution foun

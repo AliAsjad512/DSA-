8ea2fad473d094abf50f233c8d15b33f4715bf26
Ali Abbas <aa0504503@gmail.com>
2026-02-24 21:06:27 -0500

    move zeors

---
+++ python/twopoint/moveZeros.py
@@ -0,0 +1,13 @@
+ def moveZeroes(self, nums):
+        """
+        Move all zeros to end while maintaining relative order of non-zero elements.
+        
+        Approach: Slow pointer tracks position for next non-zero
+        """
+        non_zero_pos = 0
+        
+        # Move all non-zero elements to front
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[non_zero_pos], nums[i] = nums[i], nums[non_zero_pos]
+                non_zero_pos += 1

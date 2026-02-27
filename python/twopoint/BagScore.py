249192a51b4b2bb1c0882dd6b88e9584458c6a99
Ali Abbas <aa0504503@gmail.com>
2026-02-26 20:58:29 -0500

    bag of token score2

--- python/twopoint/BagScore.py
+++ python/twopoint/BagScore.py
@@ -5,3 +5,7 @@ def bagOfTokensScore(self, tokens, power):
         
         Approach: Sort + two pointers (play smallest face-up, largest face-down) [citation:6]
         """
+
+         tokens.sort()
+        left, right = 0, len(tokens) - 1
+        score = max_score = 0

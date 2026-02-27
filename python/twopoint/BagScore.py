b1373d32c486bcecd33fa92431525f9a65014217
Ali Abbas <aa0504503@gmail.com>
2026-02-26 20:59:09 -0500

    bag os Tokens score done

--- python/twopoint/BagScore.py
+++ python/twopoint/BagScore.py
@@ -9,3 +9,19 @@ def bagOfTokensScore(self, tokens, power):
          tokens.sort()
         left, right = 0, len(tokens) - 1
         score = max_score = 0
+         while left <= right:
+            if power >= tokens[left]:
+                # Play token face-up
+                power -= tokens[left]
+                score += 1
+                left += 1
+                max_score = max(max_score, score)
+            elif score > 0:
+                # Play largest token face-down
+                power += tokens[right]
+                score -= 1
+                right -= 1
+            else:
+                break
+        
+        return max_score

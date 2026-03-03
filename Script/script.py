d5524dc1f32d34ab4af7037c63c90506f9aeb471
Ali Abbas <aa0504503@gmail.com>
2026-03-02 20:39:39 -0500

    colored

--- Script/script.py
+++ Script/script.py
@@ -12,3 +12,14 @@ class GitHubPushAutomation:
         self.github_username = None
         self.repo_path = os.getcwd()
         self.branch_name = "main"
+
+def print_colored(self, text, color='green'):
+        """Print colored text for better visibility"""
+        colors = {
+            'green': '\033[92m',
+            'red': '\033[91m',
+            'yellow': '\033[93m',
+            'blue': '\033[94m',
+            'reset': '\033[0m'
+        }
+        print(f"{colors.get(color, '')}{text}{colors['reset']}")

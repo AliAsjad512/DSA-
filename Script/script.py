1c352389dec5f207dea9bba33a38623fc30c32b2
Ali Abbas <aa0504503@gmail.com>
2026-03-02 20:38:41 -0500

    script2

--- Script/script.py
+++ Script/script.py
@@ -5,3 +5,10 @@ import requests
 import json
 from pathlib import Path
 import getpass
+
+class GitHubPushAutomation:
+    def __init__(self):
+        self.repo_name = None
+        self.github_username = None
+        self.repo_path = os.getcwd()
+        self.branch_name = "main"

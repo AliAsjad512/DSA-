50495899c6f8869161a28e3d86cd991635694eeb
Ali Abbas <aa0504503@gmail.com>
2026-03-03 21:47:10 -0500

    class for monitoring healt

--- Devops/ServerHealth.py
+++ Devops/ServerHealth.py
@@ -4,3 +4,18 @@ import json
 import socket
 import os
 from typing import Dict, Any
+
+class ServerHealthMonitor:
+    def __init__(self, threshold_cpu=80, threshold_memory=80, threshold_disk=80):
+        self.threshold_cpu = threshold_cpu
+        self.threshold_memory = threshold_memory
+        self.threshold_disk = threshold_disk
+        self.hostname = socket.gethostname()
+        
+    def get_cpu_usage(self) -> Dict[str, Any]:
+        """Get CPU usage statistics"""
+        return {
+            'percent': psutil.cpu_percent(interval=1),
+            'count': psutil.cpu_count(),
+            'load_avg': psutil.getloadavg()
+        }

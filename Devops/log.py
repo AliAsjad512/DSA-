f8890904d2b68a75044fa60fff85e7727bf9883d
Ali Abbas <aa0504503@gmail.com>
2026-03-05 14:59:38 -0500

    class for logAnalyzer

--- Devops/log.py
+++ Devops/log.py
@@ -5,3 +5,16 @@ from datetime import datetime
 import os
 from typing import List, Dict, Tuple
 import argparse
+class LogAnalyzer:
+    def __init__(self, log_patterns: Dict[str, str] = None):
+        self.log_patterns = log_patterns or {
+            'ERROR': r'ERROR|error|Error|Exception|Failed|failed',
+            'WARNING': r'WARN|WARNING|warning',
+            'INFO': r'INFO|info',
+            'DEBUG': r'DEBUG|debug'
+        }
+        self.results = defaultdict(lambda: {
+            'count': 0,
+            'lines': [],
+            'timestamps': []
+        })

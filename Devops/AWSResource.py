f6619318582642ea00b04d57453390234437afed
Ali Abbas <aa0504503@gmail.com>
2026-03-16 16:05:48 -0400

    ec2 instances

--- Devops/AWSResource.py
+++ Devops/AWSResource.py
@@ -13,3 +13,21 @@ class AWSInventory:
         self.s3 = boto3.client('s3')
         self.rds = boto3.client('rds', region_name=region)
         self.resources = {}
+
+         def get_ec2_instances(self):
+        """Get all EC2 instances"""
+        instances = []
+        paginator = self.ec2.get_paginator('describe_instances')
+        for page in paginator.paginate():
+            for reservation in page['Reservations']:
+                for instance in reservation['Instances']:
+                    instances.append({
+                        'InstanceId': instance['InstanceId'],
+                        'InstanceType': instance['InstanceType'],
+                        'State': instance['State']['Name'],
+                        'LaunchTime': instance['LaunchTime'].isoformat(),
+                        'PublicIp': instance.get('PublicIpAddress', 'N/A'),
+                        'PrivateIp': instance.get('PrivateIpAddress', 'N/A'),
+                        'Tags': {t['Key']: t['Value'] for t in instance.get('Tags', [])}
+                    })
+        return instances

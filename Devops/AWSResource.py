881cd1bde87fafd8d4a3e275a2d47d752e381c4a
Ali Abbas <aa0504503@gmail.com>
2026-03-16 16:05:02 -0400

    aws inventory

--- Devops/AWSResource.py
+++ Devops/AWSResource.py
@@ -5,3 +5,11 @@ import boto3
 import json
 from datetime import datetime
 import argparse
+
+class AWSInventory:
+    def __init__(self, region='us-east-1'):
+        self.region = region
+        self.ec2 = boto3.client('ec2', region_name=region)
+        self.s3 = boto3.client('s3')
+        self.rds = boto3.client('rds', region_name=region)
+        self.resources = {}

956f6bb1ddb9e7d174db45c5eae5dd97a166487d
Ali Abbas <aa0504503@gmail.com>
2026-03-18 22:15:16 -0400

    EC2 instance

--- Devops/CI/Ansible.py
+++ Devops/CI/Ansible.py
@@ -7,3 +7,16 @@ import sys
 import json
 import boto3
 from botocore.exceptions import ClientError
+
+def get_ec2_instances(filters=None):
+    """Get EC2 instances with optional filters"""
+    ec2 = boto3.client('ec2')
+    instances = []
+    paginator = ec2.get_paginator('describe_instances')
+    for page in paginator.paginate(Filters=filters or []):
+        for reservation in page['Reservations']:
+            for instance in reservation['Instances']:
+                # Only include running instances
+                if instance['State']['Name'] == 'running':
+                    instances.append(instance)
+    return instances

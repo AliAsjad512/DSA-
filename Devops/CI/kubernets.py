f07d60e8bb9e1362ec61522c3b78e40e927ea87b
Ali Abbas <aa0504503@gmail.com>
2026-03-19 00:40:43 -0400

    k8k

--- Devops/CI/kubernets.py
+++ Devops/CI/kubernets.py
@@ -7,3 +7,22 @@ import argparse
 import time
 import datetime
 from tabulate import tabulate
+
+class K8sPodMonitor:
+    def __init__(self, namespace='default'):
+        self.namespace = namespace
+        # Load kubeconfig (assumes ~/.kube/config or in-cluster config)
+        try:
+            config.load_incluster_config()
+        except config.ConfigException:
+            config.load_kube_config()
+        self.v1 = client.CoreV1Api()
+
+    def get_pods(self):
+        """Get all pods in namespace"""
+        try:
+            pods = self.v1.list_namespaced_pod(namespace=self.namespace)
+            return pods.items
+        except ApiException as e:
+            print(f"❌ Kubernetes API error: {e}")
+            return []

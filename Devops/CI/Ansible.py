547a811d0a14414dc28df1074cf195a97eb21767
Ali Abbas <aa0504503@gmail.com>
2026-03-18 22:17:16 -0400

    ec2

--- Devops/CI/Ansible.py
+++ Devops/CI/Ansible.py
@@ -28,3 +28,39 @@ def get_ec2_instances(filters=None):
             'hostvars': {}
         }
     }
+
+    for instance in instances:
+        hostname = instance.get('PublicDnsName') or instance.get('PrivateDnsName') or instance['InstanceId']
+        host_vars = {
+            'ansible_host': instance.get('PublicIpAddress') or instance.get('PrivateIpAddress'),
+            'ec2_id': instance['InstanceId'],
+            'ec2_instance_type': instance['InstanceType'],
+            'ec2_private_ip': instance.get('PrivateIpAddress'),
+            'ec2_public_ip': instance.get('PublicIpAddress'),
+            'ec2_region': instance['Placement']['AvailabilityZone'][:-1],
+            'ec2_az': instance['Placement']['AvailabilityZone'],
+            'ec2_tags': {tag['Key']: tag['Value'] for tag in instance.get('Tags', [])}
+        }
+        inventory['_meta']['hostvars'][hostname] = host_vars
+
+        # Determine groups
+        groups = ['all']
+        # Add tag-based groups
+        tags = host_vars['ec2_tags']
+        for key, value in tags.items():
+            groups.append(f"tag_{key}_{value}")
+            groups.append(f"tag_{key}")
+        # Add instance type group
+        groups.append(f"type_{instance['InstanceType']}")
+        # Add security group groups
+        for sg in instance.get('SecurityGroups', []):
+            groups.append(f"sg_{sg['GroupName']}")
+
+        # Add host to each group
+        for group in set(groups):  # remove duplicates
+            if group not in inventory:
+                inventory[group] = {'hosts': []}
+            if hostname not in inventory[group]['hosts']:
+                inventory[group]['hosts'].append(hostname)
+
+    return inventory

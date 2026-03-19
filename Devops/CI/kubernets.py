91b664e742ed75ebc68e51c3b22e46fe7885d466
Ali Abbas <aa0504503@gmail.com>
2026-03-19 00:40:09 -0400

    kubernets

---
+++ Devops/CI/kubernets.py
@@ -0,0 +1,9 @@
+Kubernetes Pod Monitor - Watch pod status and restart counts
+"""
+
+from kubernetes import client, config
+from kubernetes.client.rest import ApiException
+import argparse
+import time
+import datetime
+from tabulate import tabulate

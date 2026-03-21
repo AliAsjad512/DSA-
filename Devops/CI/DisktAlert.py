a9fcc153aedf4d46ca7c6a905125726a9b25efcc
Ali Abbas <aa0504503@gmail.com>
2026-03-21 13:53:17 -0400

    Disk usage and email alerts

--- Devops/CI/DisktAlert.py
+++ Devops/CI/DisktAlert.py
@@ -16,3 +16,21 @@ class DiskMonitor:
         self.threshold = threshold
         self.path = path
         self.alert_sent = False  # To avoid repeated alerts
+        def check_disk_usage(self):
+        """Get disk usage percentage for given path"""
+        usage = psutil.disk_usage(self.path)
+        percent = usage.percent
+        free_gb = usage.free / (1024**3)
+        total_gb = usage.total / (1024**3)
+        return {
+            'percent': percent,
+            'free_gb': round(free_gb, 2),
+            'total_gb': round(total_gb, 2),
+            'used_gb': round(usage.used / (1024**3), 2),
+            'path': self.path
+        }
+
+    def send_email_alert(self, info, smtp_config):
+        """Send email alert"""
+        msg = MIMEText(f"""
+Disk Usage Alert!

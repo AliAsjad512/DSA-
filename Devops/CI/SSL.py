f946cf021a389240a9dfcc0ec0d4c739d804b2df
Ali Abbas <aa0504503@gmail.com>
2026-03-20 21:58:27 -0400

    get certificate

--- Devops/CI/SSL.py
+++ Devops/CI/SSL.py
@@ -22,3 +22,28 @@ class SSLChecker:
                 cert_bin = ssock.getpeercert(binary_form=True)
                 cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_ASN1, cert_bin)
                 return cert
+            
+
+            def check_expiry(self):
+        """Check certificate expiry details"""
+        cert = self.get_certificate()
+        # Get notAfter date
+        not_after_str = cert.get_notAfter().decode('ascii')
+        # Format: YYYYMMDDHHMMSSZ
+        expiry = datetime.datetime.strptime(not_after_str, '%Y%m%d%H%M%S%z')
+        now = datetime.datetime.now(datetime.timezone.utc)
+        days_left = (expiry - now).days
+
+        # Get subject and issuer
+        subject = dict(cert.get_subject().get_components())
+        issuer = dict(cert.get_issuer().get_components())
+
+        return {
+            'host': self.host,
+            'port': self.port,
+            'expiry_date': expiry.isoformat(),
+            'days_left': days_left,
+            'subject': {k.decode(): v.decode() for k, v in subject.items()},
+            'issuer': {k.decode(): v.decode() for k, v in issuer.items()},
+            'valid': days_left > 0
+        }

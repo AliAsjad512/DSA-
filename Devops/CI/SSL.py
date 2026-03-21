a6fabba6cef93a7174e6870d4e7d4d7d8b9eb873
Ali Abbas <aa0504503@gmail.com>
2026-03-20 21:57:38 -0400

    Class SSL checker

--- Devops/CI/SSL.py
+++ Devops/CI/SSL.py
@@ -8,3 +8,17 @@ import argparse
 import OpenSSL
 import certifi
 import json
+
+class SSLChecker:
+    def __init__(self, host, port=443):
+        self.host = host
+        self.port = port
+
+    def get_certificate(self):
+        """Retrieve SSL certificate from host"""
+        context = ssl.create_default_context()
+        with socket.create_connection((self.host, self.port), timeout=10) as sock:
+            with context.wrap_socket(sock, server_hostname=self.host) as ssock:
+                cert_bin = ssock.getpeercert(binary_form=True)
+                cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_ASN1, cert_bin)
+                return cert

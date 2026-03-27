a7eb8e06034696394541c6938a6294f53b22a620
Ali Abbas <aa0504503@gmail.com>
2026-03-27 17:30:41 -0400

    Database class

--- Devops/CI/database.py
+++ Devops/CI/database.py
@@ -5,3 +5,10 @@ import sys
 from pathlib import Path
 import yaml
 import logging
+
+class MigrationRunner:
+    def __init__(self, config_file='alembic.ini', env='development'):
+        self.config_file = config_file
+        self.env = env
+        self.logger = logging.getLogger(__name__)
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')

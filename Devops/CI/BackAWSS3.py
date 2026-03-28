af205fa91794f96537f294efe858ad99440e6122
Ali Abbas <aa0504503@gmail.com>
2026-03-28 19:16:13 -0400

    Class S3 Backup

--- Devops/CI/BackAWSS3.py
+++ Devops/CI/BackAWSS3.py
@@ -7,3 +7,11 @@ import shutil
 import hashlib
 import logging
 from pathlib import Path
+
+class S3Backup:
+    def __init__(self, bucket, prefix='backups/', region='us-east-1'):
+        self.bucket = bucket
+        self.prefix = prefix.rstrip('/') + '/'
+        self.s3 = boto3.client('s3', region_name=region)
+        self.logger = logging.getLogger(__name__)
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')

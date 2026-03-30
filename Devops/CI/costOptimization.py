b84451e308061bb175ba097a7d8d9862187ac5a7
Ali Abbas <aa0504503@gmail.com>
2026-03-30 16:16:10 -0400

    find idle ec2 instance

--- Devops/CI/costOptimization.py
+++ Devops/CI/costOptimization.py
@@ -15,3 +15,34 @@ class AWSCostOptimizer:
         self.elb = boto3.client('elbv2', region_name=region)
         self.rds = boto3.client('rds', region_name=region)
         self.cw = boto3.client('cloudwatch', region_name=region)
+
+        def find_idle_ec2_instances(self, days=30):
+        """Find EC2 instances with low CPU usage over last N days"""
+        instances = self.ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+        idle = []
+        for reservation in instances['Reservations']:
+            for instance in reservation['Instances']:
+                instance_id = instance['InstanceId']
+                # Get CPU utilization metrics
+                end = datetime.datetime.utcnow()
+                start = end - datetime.timedelta(days=days)
+                response = self.cw.get_metric_statistics(
+                    Namespace='AWS/EC2',
+                    MetricName='CPUUtilization',
+                    Dimensions=[{'Name': 'InstanceId', 'Value': instance_id}],
+                    StartTime=start,
+                    EndTime=end,
+                    Period=86400,  # daily
+                    Statistics=['Average']
+                )
+                if response['Datapoints']:
+                    avg_cpu = sum(dp['Average'] for dp in response['Datapoints']) / len(response['Datapoints'])
+                    if avg_cpu < 5:  # threshold
+                        idle.append({
+                            'InstanceId': instance_id,
+                            'InstanceType': instance['InstanceType'],
+                            'AverageCPU': round(avg_cpu, 2),
+                            'Name': [tag['Value'] for tag in instance.get('Tags', []) if tag['Key'] == 'Name']
+                        })
+        return idle
+        

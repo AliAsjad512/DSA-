c3dcb78040b586b0d954f1f1e3f536eafa20e833
Ali Abbas <aa0504503@gmail.com>
2026-03-30 16:15:10 -0400

    class all functions

--- Devops/CI/costOptimization.py
+++ Devops/CI/costOptimization.py
@@ -9,3 +9,9 @@ import logging
 from pathlib import Path
 
 class AWSCostOptimizer:
+    def __init__(self, region='us-east-1'):
+        self.region = region
+        self.ec2 = boto3.client('ec2', region_name=region)
+        self.elb = boto3.client('elbv2', region_name=region)
+        self.rds = boto3.client('rds', region_name=region)
+        self.cw = boto3.client('cloudwatch', region_name=region)

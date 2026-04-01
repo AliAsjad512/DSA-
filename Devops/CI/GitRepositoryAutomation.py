1ddaf52fc3ed6cb7dc156f8a1abfba85bdc031db
Ali Abbas <aa0504503@gmail.com>
2026-03-31 20:42:35 -0400

    Github class and functions

--- Devops/CI/GitRepositoryAutomation.py
+++ Devops/CI/GitRepositoryAutomation.py
@@ -3,3 +3,7 @@ import subprocess
 import argparse
 import shutil
 from pathlib import Path
+
+class GitAutomation:
+    def __init__(self, repo_path=None):
+        self.repo_path = Path(repo_path) if repo_path else Path.cwd()

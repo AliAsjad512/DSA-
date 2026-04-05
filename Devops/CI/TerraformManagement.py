009ceb1ac6b829a9bb98efae44531a943abe39ed
Ali Abbas <aa0504503@gmail.com>
2026-04-05 11:09:57 -0400

    class all functions

--- Devops/CI/TerraformManagement.py
+++ Devops/CI/TerraformManagement.py
@@ -6,3 +6,8 @@ import time
 from datetime import datetime
 
 class TerraformStateManager:
+     def __init__(self, bucket, key, region='us-east-1'):
+        self.bucket = bucket
+        self.key = key
+        self.s3 = boto3.client('s3', region_name=region)
+        self.lock_table = None  # DynamoDB table for locking

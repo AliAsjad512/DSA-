b4bf6f158a8af4d6a0aeeefebd0ac2a898a68437
Ali Abbas <aa0504503@gmail.com>
2026-04-05 11:11:00 -0400

    enable state locking and get state

--- Devops/CI/TerraformManagement.py
+++ Devops/CI/TerraformManagement.py
@@ -11,3 +11,17 @@ class TerraformStateManager:
         self.key = key
         self.s3 = boto3.client('s3', region_name=region)
         self.lock_table = None  # DynamoDB table for locking
+     def enable_state_locking(self, table_name):
+        """Enable DynamoDB state locking"""
+        self.lock_table = boto3.resource('dynamodb').Table(table_name)
+
+     def get_state(self):
+        """Download current state file"""
+        try:
+            response = self.s3.get_object(Bucket=self.bucket, Key=self.key)
+            return json.loads(response['Body'].read())
+        except self.s3.exceptions.NoSuchKey:
+            return None
+        except Exception as e:
+            print(f"❌ Error reading state: {e}")
+            return None

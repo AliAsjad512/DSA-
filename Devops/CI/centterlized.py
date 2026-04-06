bb0bac8f47cf750a760724fcf5bc45afdca49850
Ali Abbas <aa0504503@gmail.com>
2026-04-06 19:08:51 -0400

    log forward

--- Devops/CI/centterlized.py
+++ Devops/CI/centterlized.py
@@ -10,3 +10,11 @@ from pathlib import Path
 from datetime import datetime
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+
+class LogForwarder(FileSystemEventHandler):
+    def __init__(self, log_queue, log_files):
+        self.log_queue = log_queue
+        self.log_files = set(log_files)
+        self.file_handles = {}
+        self.file_positions = {}
+        self._init_tracking()

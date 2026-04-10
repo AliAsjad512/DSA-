fee111d42bc7c1f683f646f9b7938c808aa184c5
Ali Abbas <aa0504503@gmail.com>
2026-04-09 21:06:48 -0400

    Service Health Monitor

--- Devops/CI/ServiceHealthCheck.py
+++ Devops/CI/ServiceHealthCheck.py
@@ -6,3 +6,12 @@ import sys
 from datetime import datetime
 import requests
 import socket
+
+
+class ServiceHealthMonitor:
+    def __init__(self, service_name, check_interval=30, max_failures=3):
+        self.service_name = service_name
+        self.check_interval = check_interval
+        self.max_failures = max_failures
+        self.failure_count = 0
+        self.setup_logging()

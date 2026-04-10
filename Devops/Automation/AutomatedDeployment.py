494031c2174813266bc75a5ae2d01325a5e5e84e
Ali Abbas <aa0504503@gmail.com>
2026-04-10 12:03:33 -0400

    Deployment Manager class

--- Devops/Automation/AutomatedDeployment.py
+++ Devops/Automation/AutomatedDeployment.py
@@ -6,3 +6,13 @@ import os
 import shutil
 import requests
 from datetime import datetime
+
+class DeploymentManager:
+    def __init__(self, app_name, deploy_dir='/var/www', backup_dir='/var/backups'):
+        self.app_name = app_name
+        self.deploy_dir = Path(deploy_dir) / app_name
+        self.backup_dir = Path(backup_dir) / app_name
+        self.current_symlink = self.deploy_dir / 'current'
+        self.releases_dir = self.deploy_dir / 'releases'
+        self.backup_dir.mkdir(parents=True, exist_ok=True)
+        self.releases_dir.mkdir(parents=True, exist_ok=True)

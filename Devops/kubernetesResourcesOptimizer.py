c6ea55895f1f212a354cb3501aa85d2ed8010989
Ali Abbas <aa0504503@gmail.com>
2026-04-11 09:35:38 -0400

    class kubernetes

--- Devops/kubernetesResourcesOptimizer.py
+++ Devops/kubernetesResourcesOptimizer.py
@@ -2,3 +2,10 @@ from kubernetes import client, config
 import argparse
 import json
 from datetime import datetime
+
+class K8sResourceOptimizer:
+    def __init__(self, namespace='default'):
+        self.namespace = namespace
+        config.load_kube_config()
+        self.core_v1 = client.CoreV1Api()
+        self.metrics_v1 = client.CustomObjectsApi()

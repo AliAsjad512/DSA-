dda7f0ab68fb390a9931d0d6e45916fec158a16f
Ali Abbas <aa0504503@gmail.com>
2026-04-11 09:36:16 -0400

    pod resources

--- Devops/kubernetesResourcesOptimizer.py
+++ Devops/kubernetesResourcesOptimizer.py
@@ -9,3 +9,24 @@ class K8sResourceOptimizer:
         config.load_kube_config()
         self.core_v1 = client.CoreV1Api()
         self.metrics_v1 = client.CustomObjectsApi()
+
+     def get_pod_resources(self):
+        """Get all pods with their resource requests and limits"""
+        pods = self.core_v1.list_namespaced_pod(self.namespace)
+        results = []
+        for pod in pods:
+            if pod.status.phase != 'Running':
+                continue
+            for container in pod.spec.containers:
+                resources = container.resources
+                req = resources.requests if resources and resources.requests else {}
+                lim = resources.limits if resources and resources.limits else {}
+                results.append({
+                    'pod': pod.metadata.name,
+                    'container': container.name,
+                    'cpu_request': req.get('cpu', '0'),
+                    'cpu_limit': lim.get('cpu', '0'),
+                    'memory_request': req.get('memory', '0'),
+                    'memory_limit': lim.get('memory', '0')
+                })
+        return results

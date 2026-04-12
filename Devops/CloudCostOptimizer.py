db1a0d76cca71c23ba6ab97090d29db6f32d41f2
Ali Abbas <aa0504503@gmail.com>
2026-04-12 14:37:34 -0400

    Cost Anomaly Detector

--- Devops/CloudCostOptimizer.py
+++ Devops/CloudCostOptimizer.py
@@ -4,3 +4,7 @@ import json
 import argparse
 import numpy as np
 from collections import defaultdict
+
+class CostAnomalyDetector:
+    def __init__(self, region='us-east-1'):
+        self.ce = boto3.client('ce', region_name=region)

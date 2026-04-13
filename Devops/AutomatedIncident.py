5d392dd1b78c8ac62f1ace9f7b59886c838cc3ff
Ali Abbas <aa0504503@gmail.com>
2026-04-13 12:42:13 -0400

    Incdident responder class

--- Devops/AutomatedIncident.py
+++ Devops/AutomatedIncident.py
@@ -5,3 +5,9 @@ import time
 import requests
 import logging
 from pathlib import Path
+
+class IncidentResponder:
+    def __init__(self, runbook_dir='./runbooks'):
+        self.runbook_dir = Path(runbook_dir)
+        self.logger = logging.getLogger(__name__)
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')

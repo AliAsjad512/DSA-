9b89ddceeacfa6c19746bb1727fe1a111686629f
Ali Abbas <aa0504503@gmail.com>
2026-04-14 09:20:09 -0400

    Class Drift Detector

--- Devops/infrastructureDriftDetector.py
+++ Devops/infrastructureDriftDetector.py
@@ -3,3 +3,12 @@ import json
 import argparse
 import boto3
 from deepdiff import DeepDiff
+
+class DriftDetector:
+    def __init__(self, tf_state_path='terraform.tfstate'):
+        self.tf_state_path = tf_state_path
+        self.tf_state = self._load_tf_state()
+
+    def _load_tf_state(self):
+        with open(self.tf_state_path, 'r') as f:
+            return json.load(f)

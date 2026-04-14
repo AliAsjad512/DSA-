997fd015acda242b0c9f547598ba40e74ca53310
Ali Abbas <aa0504503@gmail.com>
2026-04-14 09:20:47 -0400

    get resources From tf

--- Devops/infrastructureDriftDetector.py
+++ Devops/infrastructureDriftDetector.py
@@ -12,3 +12,15 @@ class DriftDetector:
     def _load_tf_state(self):
         with open(self.tf_state_path, 'r') as f:
             return json.load(f)
+    def get_resources_from_tf(self, resource_type):
+        """Extract resources of a specific type from Terraform state"""
+        resources = []
+        for resource in self.tf_state.get('resources', []):
+            if resource['type'] == resource_type:
+                for instance in resource.get('instances', []):
+                    resources.append({
+                        'id': instance['attributes'].get('id'),
+                        'attributes': instance['attributes']
+                    })
+        return resources
+

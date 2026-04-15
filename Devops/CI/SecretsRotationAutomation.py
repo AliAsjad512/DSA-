01df30352f208860cc1d2fd8ca69f29d588b64d6
Ali Abbas <aa0504503@gmail.com>
2026-04-15 08:32:01 -0400

    Class Secrets Rotator

--- Devops/CI/SecretsRotationAutomation.py
+++ Devops/CI/SecretsRotationAutomation.py
@@ -5,3 +5,10 @@ import json
 import argparse
 import logging
 from datetime import datetime
+
+class SecretsRotator:
+    def __init__(self, region='us-east-1'):
+        self.sm = boto3.client('secretsmanager', region_name=region)
+        self.rds = boto3.client('rds', region_name=region)
+        self.logger = logging.getLogger(__name__)
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')

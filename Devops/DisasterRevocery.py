cf36bc4166b60dbe487b1572ee419f0faf134e74
Ali Abbas <aa0504503@gmail.com>
2026-04-16 09:58:09 -0400

    Disastory Recovery

--- Devops/DisasterRevocery.py
+++ Devops/DisasterRevocery.py
@@ -3,3 +3,10 @@ import argparse
 import time
 import logging
 import json
+class DisasterRecovery:
+    def __init__(self, primary_region='us-east-1', dr_region='us-west-2'):
+        self.primary = primary_region
+        self.dr = dr_region
+        self.route53 = boto3.client('route53')
+        self.logger = logging.getLogger(__name__)
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')

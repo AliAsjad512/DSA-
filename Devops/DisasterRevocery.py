5c06c54f68da47829e84232a384060d8b603b107
Ali Abbas <aa0504503@gmail.com>
2026-04-16 09:58:45 -0400

    Check primary health

--- Devops/DisasterRevocery.py
+++ Devops/DisasterRevocery.py
@@ -10,3 +10,12 @@ class DisasterRecovery:
         self.route53 = boto3.client('route53')
         self.logger = logging.getLogger(__name__)
         logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+        def check_primary_health(self, health_check_url):
+        """Simple health check for primary region"""
+        import requests
+        try:
+            resp = requests.get(health_check_url, timeout=10)
+            return resp.status_code == 200
+        except:
+            return False

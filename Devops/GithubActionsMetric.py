66d1d70c19113dc2daf4ea7319302096e0cf8290
Ali Abbas <aa0504503@gmail.com>
2026-04-17 08:51:59 -0400

    GHAMetrics class

--- Devops/GithubActionsMetric.py
+++ Devops/GithubActionsMetric.py
@@ -3,3 +3,9 @@ import argparse
 import json
 import datetime
 from collections import defaultdict
+
+class GHAMetrics:
+    def __init__(self, repo, token):
+        self.repo = repo
+        self.headers = {'Authorization': f'token {token}', 'Accept': 'application/vnd.github.v3+json'}
+        self.base_url = f'https://api.github.com/repos/{repo}'

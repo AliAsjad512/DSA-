2381b8e5b789efa5f243102176bd088c3e7fb631
Ali Abbas <aa0504503@gmail.com>
2026-04-18 19:34:03 -0400

    JenkinsJobBuilder

--- Devops/CI/customJenkin.py
+++ Devops/CI/customJenkin.py
@@ -3,3 +3,7 @@ import yaml
 import argparse
 import os
 from pathlib import Path
+
+class JenkinsJobBuilder:
+    def __init__(self, url, username, password):
+        self.server = jenkins.Jenkins(url, username=username, password=password)
